e4e597ac3cfe1cd09bde5f356c8635cbc33b42ed
mine9607 <81383389+mine9607@users.noreply.github.com>
2025-01-30 22:01:20 -0500

    Finished the simplistic version of a lexer to cover keywords, identifiers, operators and delimiters

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -23,10 +23,17 @@ func (l *Lexer) readChar() {
 	}
 	l.position = l.readPosition
 	l.readPosition += 1
+
+	// Note we need to add functionality that
+	// 1 - check if current char is a letter or number
+	// 2 - if a letter, continue reading until the next non-letter value
+	// 3 - determine if the final string is an "identifier" or a "keyword" (i.e. 'fn')
 }
 
 func (l *Lexer) NextToken() token.Token {
 	var tok token.Token
+
+	l.skipWhitespace()
 
 	switch l.ch {
 	case '=':
@@ -48,12 +55,61 @@ func (l *Lexer) NextToken() token.Token {
 	case 0:
 		tok.Literal = ""
 		tok.Type = token.EOF
+	default:
+		if isLetter(l.ch) {
+			tok.Literal = l.readIdentifier()
+			tok.Type = token.LookupIdent(tok.Literal)
+			return tok
+		} else if isDigit(l.ch) {
+			tok.Type = token.INT
+			tok.Literal = l.readNumber()
+			return tok
+		} else {
+			tok = newToken(token.ILLEGAL, l.ch)
+		}
 	}
 
 	l.readChar()
 	return tok
 }
 
+// func takes a tokenType and a char and returns a Token struct
 func newToken(tokenType token.TokenType, ch byte) token.Token {
 	return token.Token{Type: tokenType, Literal: string(ch)}
+}
+
+// method on the Lexer pointer which assigns the position to the current lexer position value
+// if that value is a letter then it call readChar() to increment the postions being read
+// returns a slice of the input from the current position to the new position
+func (l *Lexer) readIdentifier() string {
+	position := l.position
+	for isLetter(l.ch) {
+		l.readChar()
+	}
+	return l.input[position:l.position]
+}
+
+// func which accepts a char and returns a bool indicating if the char is a letter
+// NOTE: we also include '_' as a letter to make names like foo_bar
+// NOTE: if we wanted to include other letters in our named identifiers/keywords we could add here ('?', '!', etc.)
+func isLetter(ch byte) bool {
+	return 'a' <= ch && ch <= 'z' || 'A' <= ch && ch <= 'Z' || ch == '_'
+}
+
+func (l *Lexer) skipWhitespace() {
+	for l.ch == ' ' || l.ch == '\t' || l.ch == '\n' || l.ch == '\r' {
+		l.readChar()
+	}
+}
+
+func (l *Lexer) readNumber() string {
+	position := l.position
+	for isDigit(l.ch) {
+		l.readChar()
+	}
+	return l.input[position:l.position]
+}
+
+func isDigit(ch byte) bool {
+	return '0' <= ch && ch <= '9'
 }
